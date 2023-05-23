b8b9a2c28e6308a1d3c677b28cbe255ed3894bbd
danio86 <>
2023-05-23 13:42:27 +0000

    add put request ti views

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
+from django.http import Http404
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Profile
 from .serializers import ProfileSerializer
 
 
@@ -10,3 +15,29 @@ class ProfileList(APIView):
         serializer = ProfileSerializer(profiles, many=True)
         # es können mehrere profiles sein!
         return Response(serializer.data)
+
+
+class ProfileDetail(APIView):
+    serializer_class = ProfileSerializer
+    # das macht in der rest framework ansicht eine schönere Form
+    def get_object(self, pk):
+        try:
+            profile = Profile.objects.get(pk=pk)
+            return profile
+        except Profile.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile)
+        # diesmal verarbeite ich ein einzelnes Profil und kein Queryset
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        # ist wie edit oder add vorzustellen
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
