07a461f56b0c57ae6ba16b4fde5b95893b22775b
danio86 <>
2023-05-23 15:27:38 +0000

    add post app

---
+++ posts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from posts import views
+
+
+urlpatterns = [
+    path('posts/', views.PostList.as_view()),
+    path('posts/<int:pk>/', views.PostsDetail.as_view()),
+]
