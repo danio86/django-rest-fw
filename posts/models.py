8d7bcbee25fb8307aa312f27a0ced72e348fd09a
danio86 <>
2023-05-23 17:29:15 +0000

    add image validator in the post serializer

--- posts/models.py
+++ posts/models.py
@@ -7,15 +7,38 @@ class Post(models.Model):
     Post model, related to 'owner', i.e. a User instance.
     Default image set so that we can always reference image.url.
     """
+    image_filter_choices = [
+        ('_1977', '1977'),
+        ('brannan', 'Brannan'),
+        ('earlybird', 'Earlybird'),
+        ('hudson', 'Hudson'),
+        ('inkwell', 'Inkwell'),
+        ('lofi', 'Lo-Fi'),
+        ('kelvin', 'Kelvin'),
+        ('normal', 'Normal'),
+        ('nashville', 'Nashville'),
+        ('rise', 'Rise'),
+        ('toaster', 'Toaster'),
+        ('valencia', 'Valencia'),
+        ('walden', 'Walden'),
+        ('xpro2', 'X-pro II')
+    ]
+
+    # he first value is the actual string saved in the database and
+    # the second one is the value  displayed in the dropdown menu
+    # in the in -browser interface.
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
     image = models.ImageField(
-        upload_to='images/', default='../woman-g97a82b1d8_640_eblkq9',
-        blank=True
+        upload_to='images/', default='../default_post_rgq6aq', blank=True
     )
+    image_filter = models.CharField(
+        max_length=32, choices=image_filter_choices, default='normal'
+    )
+    # kein filter wird angewended außer wir ändern "normal"
 
     class Meta:
         ordering = ['-created_at']
