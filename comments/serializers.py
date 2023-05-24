b4bc212de52c1366bcca4cf1c6162bc40288fce7
danio86 <>
2023-05-24 14:35:49 +0000

    add comments generic view

--- comments/serializers.py
+++ comments/serializers.py
@@ -8,19 +8,6 @@ class CommentSerializer(serializers.ModelSerializer):
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
 
-    # def validate_image(self, value):
-    #     if value.size > 2 * 1024 * 1024:
-    #         raise serializers.ValidationError('Image size larger than 2MB!')
-    #     if value.image.height > 4096:
-    #         raise serializers.ValidationError(
-    #             'Image height larger than 4096px!'
-    #         )
-    #     if value.image.width > 4096:
-    #         raise serializers.ValidationError(
-    #             'Image width larger than 4096px!'
-    #         )
-    #     return value
-
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
@@ -36,9 +23,10 @@ class CommentSerializer(serializers.ModelSerializer):
 
 class CommentDetailSerializer(CommentSerializer):
 
+    """
+    Serializer for the Comment model used in Detail view
+    Post is a read only field so that we dont have to set it on each update
+    """
 
-"""
-Serializer for the Comment model used in Detail view
-Post is a read only field so that we dont have to set it on each update
-"""
+
 post = serializers.ReadOnlyField(source='post.id')
