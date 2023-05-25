dbad11768ce707285ca1716e516e6bc3316f0632
danio86 <>
2023-05-25 16:10:22 +0000

    add count fiels and filter to post view and sterializers

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics, permissions
+from django.db.models import Count
+from rest_framework import generics, permissions, filters
 from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Post
 from .serializers import PostSerializer
@@ -11,7 +12,18 @@ class PostList(generics.ListCreateAPIView):
     """
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    queryset = Post.objects.all()
+    queryset = Post.objects.annotate(
+        likes_count=Count('likes', distinct=True),
+        comments_count=Count('comment', distinct=True)
+    ).order_by('-created_at')
+    filter_backends = [
+        filters.OrderingFilter
+    ]
+    ordering_fields = [
+        'likes_count',
+        'comments_count',
+        'likes__created_at',
+    ]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -23,7 +35,10 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     serializer_class = PostSerializer
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Post.objects.all()
+    queryset = Post.objects.annotate(
+        likes_count=Count('likes', distinct=True),
+        comments_count=Count('comment', distinct=True)
+    ).order_by('-created_at')
 
 
 # from django.http import Http404
@@ -97,5 +112,4 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
 #         post = self.get_object(pk)
 #         post.delete()
 #         return Response(
-#             status=status.HTTP_204_NO_CONTENT
-#         )
+#             status=status.HTTP_204_NO_CONTENT)
--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,5 @@
 from django.db.models import Count
-from rest_framework import generics, filters
+from rest_framework import generics, permissions, filters
 from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
