d9e18ecf30aee8554b51fe5a7213d5b63886ffcb
danio86 <>
2023-05-25 15:36:48 +0000

    add count fields to profile view and styrializer

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,3 +1,4 @@
+from followers.models import Follower
 from rest_framework import serializers
 from .models import Profile
 
@@ -5,16 +6,35 @@ from .models import Profile
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    following_id = serializers.SerializerMethodField()
+    # das fügt ein weiters api feld hinzu
+    posts_count = serializers.ReadOnlyField()
+    followers_count = serializers.ReadOnlyField()
+    following_count = serializers.ReadOnlyField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
-        # checkt user ownership. 
+        # checkt user ownership.
         # Die Argumente müssen über views mitgeliefert werden.
+
+    def get_following_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            following = Follower.objects.filter(
+                owner=user, followed=obj.owner
+                # checks if the loggedin user is following this profile
+            ).first()
+            # print(following, 'test')
+            # das gergibt das ana dan folgt oder none
+            return following.id if following else None
+        return None
+        # dieser Block checkt on der user einen autentifizierten Follower hat.
 
     class Meta:
         model = Profile
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'name',
-            'content', 'image', 'is_owner'
+            'content', 'image', 'is_owner', 'following_id',
+            'posts_count', 'followers_count', 'following_count',
         ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics
+from django.db.models import Count
+from rest_framework import generics, filters
 from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
@@ -9,8 +10,43 @@ class ProfileList(generics.ListAPIView):
     List all profiles.
     No create view as profile creation is handled by django signals.
     """
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        # da in Follow modell zwei foreign keys sind nehmen wir den related name
+        # nicht das Model
+        following_count=Count('owner__following', distinct=True)
+        # __ stellt die verbindung 2er Modelle her, die nicht direkt, aber
+        # über das USER-Modell verbunden sind.
+        # Da wir mehr als ein Feld innerhalb der annotate-Funktion definieren
+        # werden, müssen wir hier auch distinct=True übergeben, um nur die
+        # unique Beiträge zu zählen. Ohne dies würden wir Duplikate erhalten.
+
+        # die drei müssen auch in den styrealizer!
+    ).order_by('-created_at')
     serializer_class = ProfileSerializer
+    filter_backends = [
+        filters.OrderingFilter
+    ]
+    ordering_fields = [
+        'posts_count',
+        'followers_count',
+        'following_count',
+        'owner__following__created_at',
+        'owner__followed__created_at',
+        # created_at ist so attached to followed/ing, also zu anderen
+        # attributen im selben model und zu owner (anderes Model)
+    ]
+    # Als Nächstes müssen wir unsere Filter erstellen. Um diese Felder
+    # sortierbar zu machen, werde ich das Attribut filter_backends auf
+    # OrderingFilter setzen. Außerdem muss ich die ordering_fields auf die
+    # Felder setzen, die wir gerade annotiert haben, nämlich die Anzahl der
+    # Beiträge, Follower und Personen, denen gefolgt wird.
+
+    # Die annotate-Funktion ermöglicht es uns, zusätzliche Felder zu definieren
+    # die dem Queryset hinzugefügt werden sollen. In unserem Fall werden wir
+    # Felder hinzufügen, um herauszufinden, wie viele Beiträge und Follower ein
+    # Benutzer hat und wie vielen anderen Benutzern sie folgen.
 
 
 class ProfileDetail(generics.RetrieveUpdateAPIView):
@@ -18,8 +54,37 @@ class ProfileDetail(generics.RetrieveUpdateAPIView):
     Retrieve or update a profile if you're the owner.
     """
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_at')
+    # queryset ist nur von ober koppiert um die neu erstellten Felder auch
+    # in einem einzelnen Profil zu sehen.
     serializer_class = ProfileSerializer
+
+# from rest_framework import generics
+# from drf_api.permissions import IsOwnerOrReadOnly
+# from .models import Profile
+# from .serializers import ProfileSerializer
+
+
+# class ProfileList(generics.ListAPIView):
+#     """
+#     List all profiles.
+#     No create view as profile creation is handled by django signals.
+#     """
+#     queryset = Profile.objects.all()
+#     serializer_class = ProfileSerializer
+
+
+# class ProfileDetail(generics.RetrieveUpdateAPIView):
+#     """
+#     Retrieve or update a profile if you're the owner.
+#     """
+#     permission_classes = [IsOwnerOrReadOnly]
+#     queryset = Profile.objects.all()
+#     serializer_class = ProfileSerializer
 
 
 # from django.http import Http404
