10cf8ea6ef499e4efbfd1d5e7846a38c4374f271
danio86 <>
2023-05-26 14:46:10 +0000

    add heroku varibale for allowed host

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -8,8 +8,10 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
+
 """
 
+import re
 from pathlib import Path
 import os
 import dj_database_url
@@ -61,7 +63,9 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [
-    'localhost', 'django-rest-fw.herokuapp.com', '8000-danio86-django-rest-fw-jq0q3auumw.us2.codeanyapp.com']
+    'localhost', os.environ.get('ALLOWED_HOST'),
+    '8000-danio86-django-rest-fw-jq0q3auumw.us2.codeanyapp.com',
+]
 
 
 # Application definition
@@ -104,13 +108,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-if 'CLIENT_ORIGIN' in os.environ:
-    CORS_ALLOWED_ORIGINS = [
-        os.environ.get('CLIENT_ORIGIN')
-    ]
-else:
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(
+        r'^([^.]+)', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
+
     CORS_ALLOWED_ORIGIN_REGEXES = [
-        r"^https://.*\.gitpod\.io$",
+        rf"{extracted_url}.(eu|us)\d+\.codeanyapp\.com$",
     ]
 
 CORS_ALLOW_CREDENTIALS = True
