d7d7460e95b7be65f5cb30d703e9a1182d18a31f
danio86 <>
2023-08-16 17:34:52 +0000

    changed propertie initial.py operations name

--- properties/migrations/0001_initial.py
+++ properties/migrations/0001_initial.py
@@ -15,7 +15,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Post',
+            name='Property',
             fields=[
                 ('id', models.BigAutoField(auto_created=True,
                  primary_key=True, serialize=False, verbose_name='ID')),
